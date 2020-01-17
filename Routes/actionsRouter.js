5c8552e72c166df0ac347c7de7177663d85cacb1
Bryan <17adamsbryk@washk12.org>
2020-01-17 09:51:03 -0700

    delete by action ID, actionID requests need middleware so they only modify actionIDs that belong to params ProjectID

--- Routes/actionsRouter.js
+++ Routes/actionsRouter.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
-
 //PARAMS DOESN'T GET PASSED DOWN UNLESS U LINK THEM
 
 //https://stackoverflow.com/questions/25260818/rest-with-express-js-nested-router
 const router = express.Router({
-    mergeParams: true
+  mergeParams: true
 });
 
 const Actions = require("../data/helpers/actionModel");
@@ -25,21 +24,37 @@ router.get("/:actionID", (req, res) => {
 });
 //POST REQUESTS
 router.post("/", (req, res) => {
-    Actions.insert({
-      description: req.body.description,
-      notes: req.body.notes,
-      project_id: req.params.id,
-      completed: req.body.completed
+  Actions.insert({
+    description: req.body.description,
+    notes: req.body.notes,
+    project_id: req.params.id,
+    completed: req.body.completed
+  })
+    .then(action => {
+      res.status(201).json(action);
     })
-      .then(action => {
-        res.status(201).json(action);
-      })
-      .catch(err => {
-        res.status(500).json({
-          errorMessage:
-            "Could not add action. This might mean the Project doesn't exist"
-        });
+    .catch(err => {
+      res.status(500).json({
+        errorMessage:
+          "Could not add action. This might mean the Project doesn't exist"
       });
-  });
+    });
+});
+
+//DELETE REQUESTS
+router.delete("/:actionID", (req, res) => {
+  Actions.remove(req.params.actionID)
+    .then(action => {
+      res.status(200).json({
+        message: `Action ${req.params.actionID} was successfully deleted`,
+        deletedID: parseInt(req.params.actionID)
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "error deleting action"
+      });
+    });
+});
 
 module.exports = router;

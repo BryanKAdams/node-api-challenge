1400f4d9f0075706a601a1ddd7e17ec533fb2f04
Bryan <17adamsbryk@washk12.org>
2020-01-17 09:18:53 -0700

    Created GET by specific Project same as the top one but passing in parameter and doing a check that we get anything passed back

--- Routes/projectsRouter.js
+++ Routes/projectsRouter.js
@@ -8,17 +8,38 @@ const Projects = require("../data/helpers/projectModel");
 
 // GET /api/projects returns a list of all projects back
 router.get("/", (req, res) => {
-    Projects.get()
-      .then(projects => {
-        res.status(200).json(projects);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          errorMessage: "The projects list could not be retrieved."
-        });
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The projects list could not be retrieved."
       });
-  });
+    });
+});
 
+//GET /api/projects/:id returns a specific project back
+// exact same thing as above request but passing parameter in and checking
+// if that project exists
+router.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+    .then(projects => {
+      if (projects) {
+        res.status(200).json(projects);
+      } else {
+        res.status(400).json({
+          errorMessage: "project with the specified ID does not exist"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The projects list could not be retrieved."
+      });
+    });
+});
 
 module.exports = router;

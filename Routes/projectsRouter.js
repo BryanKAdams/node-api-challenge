80f4ef1b8750bd4c9f172e8d2feab75dd330023a
Bryan <17adamsbryk@washk12.org>
2020-01-17 09:21:09 -0700

    Get list of actions by specified Project ID, created custom middleware which just does a git request of the Projects url and checks if they exist before checking to see if they have Actions

--- Routes/projectsRouter.js
+++ Routes/projectsRouter.js
@@ -42,4 +42,50 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//GET /api/projects/:id/actions returns a specific projects actions back
+router.get("/:id/actions", projectExists, (req, res) => {
+  Projects.getProjectActions(req.params.id)
+
+    .then(actions => {
+      if (actions.length > 0) {
+        res.status(200).json(actions);
+      } else if (actions.length == 0) {
+        res.status(400).json({
+          errorMessage: "This project has no actions"
+        });
+      } else {
+        res.status(400).json({
+          errorMessage: "The project with the specified ID does not exist"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The project could not be retrieved."
+      });
+    });
+});
+
+//custom middleware
+
+function projectExists(req, res, next) {
+  Projects.get(req.params.id)
+    .then(projects => {
+      if (projects) {
+        next();
+      } else {
+        res.status(400).json({
+          errorMessage: "project with the specified ID does not exist"
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The projects list could not be retrieved."
+      });
+    });
+}
+
 module.exports = router;

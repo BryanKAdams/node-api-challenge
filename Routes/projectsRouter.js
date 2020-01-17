42b8c336754330282374f6007ab81e4334ac4641
Bryan <17adamsbryk@washk12.org>
2020-01-17 09:16:43 -0700

    Created Get Requests section and created basic GET request to base projects URL

--- Routes/projectsRouter.js
+++ Routes/projectsRouter.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 const Projects = require("../data/helpers/projectModel");
 
+//GET REQUESTS
 
+// GET /api/projects returns a list of all projects back
+router.get("/", (req, res) => {
+    Projects.get()
+      .then(projects => {
+        res.status(200).json(projects);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          errorMessage: "The projects list could not be retrieved."
+        });
+      });
+  });
 
 
 module.exports = router;

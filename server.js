cbd8a3d1bcdfbf30d7f9139451f1c133652a66c2
Bryan <17adamsbryk@washk12.org>
2020-01-17 09:13:25 -0700

    Created server.js and filled it with nonexisting server files for now

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const cors = require('cors');
+const projectsRouter = require("./projects/projectsRouter");
+const actionsRouter = require("./actions/actionsRouter.js");
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+server.get("/", logger, (req, res) => {
+  res.send("<h2> Check out /api/projects or /api/projects/:id/actions</h2>");
+});
+server.use("/api/projects", projectsRouter);
+server.use("/api/projects/:id/actions", actionsRouter);
+
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
+module.exports = server;

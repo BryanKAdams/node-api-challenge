52dc5c594189f6c88871227d16fb6fcf5ef061ff
Bryan <17adamsbryk@washk12.org>
2020-01-17 09:40:40 -0700

    Created get request for actionID but need to restore database to test

--- Routes/actionsRouter.js
+++ Routes/actionsRouter.js
@@ -4,5 +4,18 @@ const router = express.Router();
 
 const Actions = require("../data/helpers/actionModel");
 
+//GET REQUESTS
+router.get("/:actionID", (req, res) => {
+  Actions.get(req.params.actionID)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        errorMessage: "The actions list could not be retrieved."
+      });
+    });
+});
 
 module.exports = router;
